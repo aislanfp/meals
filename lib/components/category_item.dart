45c0a67fb8d16c2d57a3bf8355c5e247d0683788
Aislan <aislanfp@users.noreply.github.com>
2022-08-17 17:22:15 -0300

    Aula 182

---
+++ lib/components/category_item.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:meals/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryItem(this.category, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+      child: Text(category.title),
+    );
+  }
+}
